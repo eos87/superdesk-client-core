3d151e87e3c9b9b81e3edde0ccf6196f1b73f5ad
pavlovicnemanja <me@pavlovicnemanja.com>
2018-05-14 16:24:53 +0200

    feat(image-editor): Improvements for multi metadata image editor

--- scripts/apps/search/services/MultiImageEditService.js
+++ scripts/apps/search/services/MultiImageEditService.js
@@ -4,26 +4,58 @@ export function MultiImageEditService($modal, authoring) {
         $modal.open({
             templateUrl: 'scripts/apps/search/views/multi-image-edit.html',
             controller: ['$scope', 'config', function ($scope, config) {
-                $scope.images = data;
-                $scope.validator = config.validatorMediaMetadata;
+                    $scope.images = _.clone(data);
 
-                $scope.metadata = {
-                    headline: 'Test',
-                    description_text: 'Test',
-//                    archive_description: 'Test',
-//                    alt_text: 'Test',
-//                    byline: 'Test',
-//                    copyrightholder: 'Test',
-//                    usageterms: 'Test',
-//                    copyrightnotice: 'Test'
-                };
+                    $scope.validator = config.validatorMediaMetadata;
 
-                $scope.save = () => {
-                    angular.forEach($scope.images, (image) => {
-                        authoring.save(image, $scope.metadata);
-                    });
-                };
-            }],
+                    $scope.placeholders = {};
+
+                    $scope.metadata = {
+                        headline: compare('headline'),
+                        description_text: compare('description_text'),
+                        archive_description: compare('archive_description'),
+                        alt_text: compare('alt_text'),
+                        byline: compare('byline'),
+                        copyrightholder: compare('copyrightholder'),
+                        usageterms: compare('usageterms'),
+                        copyrightnotice: compare('copyrightnotice')
+                    };
+
+                    $scope.save = (close) => {
+                        let metadata = _.pickBy($scope.metadata, (meta) => {
+                            return meta !== '';
+                        });
+
+                        angular.forEach($scope.images, (image) => {
+                            if (!image.unselected) {
+                                authoring.save(image, metadata);
+                            }
+                        });
+
+                        if (close) {
+                            $scope.$close();
+                        }
+                    };
+
+                    function compare(value) {
+                        let uniqueValue = true;
+
+                        angular.forEach($scope.images, (image) => {
+                            if ($scope.images[0][value] !== image[value]) {
+                                uniqueValue = false;
+                            }
+                        });
+
+                        if (uniqueValue) {
+                            return $scope.images[0][value];
+                        }
+
+                        $scope.placeholders[value] = '(multiple values)';
+
+                        return '';
+                    }
+                }],
+
             size: 'fullscreen modal--dark-ui'
         });
     };
